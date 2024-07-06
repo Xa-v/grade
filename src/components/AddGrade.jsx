c4bde679e401b8f8a714abb0ef3eee6dc3ddbac5
semajame <jamesanquillano@gmail.com>
2024-07-06 23:56:19 +0800

    update

--- src/components/AddGrade.jsx
+++ src/components/AddGrade.jsx
@@ -1,52 +1,45 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const AddGrade = () => {
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:4000/grades', { name });
+      const response = await axios.post("http://localhost:4000/grades", {
+        name,
+      });
       setMessage(response.data.message);
-      setName(''); // Clear the input field
+      setName(""); // Clear the input field
     } catch (error) {
-      setMessage('Error adding grade');
-      console.error('There was an error!', error);
+      setMessage("Error adding grade");
+      console.error("There was an error!", error);
     }
   };
 
   return (
     <div>
-    <h2>Add Student</h2>
+      <h2>Add Student</h2>
 
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          class="form-control mb-3 w-50"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
 
- <form onSubmit={handleSubmit}>
-      
-<label>Name:</label>
-<input 
-  type="text"
-  class="form-control mb-3 w-50"
-  value={name}
-  onChange={(e) => setName(e.target.value)}
-  required
-/>
-
-
-
-<button type="submit" class="btn btn-primary">Add</button>
-</form>
-{message && <p>{message}</p>} 
-
-
-
-  
+        <button type="submit" class="btn btn-primary">
+          Add
+        </button>
+      </form>
+      {message && <p>{message}</p>}
     </div>
   );
 };
 
 export default AddGrade;
-
-
-
--- src/components/GetAttendance.jsx
+++ src/components/GetAttendance.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Grades = () => {
   const [grades, setGrades] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [formData, setFormData] = useState({});
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const fetchGrades = async () => {
       try {
-        const response = await axios.get('http://localhost:4000/grades');
+        const response = await axios.get("http://localhost:4000/grades");
         setGrades(response.data);
         setLoading(false);
       } catch (err) {
@@ -23,6 +23,9 @@ const Grades = () => {
 
     fetchGrades();
   }, []);
+  const inputNumber = (event) => {
+    event.target.value = event.target.value.replace(/\D/g, "");
+  };
 
   const handleInputChange = (e, gradeid) => {
     const { name, value } = e.target;
@@ -30,28 +33,28 @@ const Grades = () => {
       ...formData,
       [gradeid]: {
         ...formData[gradeid],
-        [name]: value
-      }
+        [name]: value,
+      },
     });
   };
 
   const handleInputFocus = (e) => {
     e.target.select();
-
-    
   };
 
   const handleSave = async (gradeid) => {
     try {
-      const response = await axios.put(`http://localhost:4000/grades/${gradeid}`, formData[gradeid]);
+      const response = await axios.put(
+        `http://localhost:4000/grades/${gradeid}`,
+        formData[gradeid]
+      );
       setMessage(response.data.message);
       // Refresh grades data after update
-      const updatedGrades = await axios.get('http://localhost:4000/grades');
+      const updatedGrades = await axios.get("http://localhost:4000/grades");
       setGrades(updatedGrades.data);
-   
     } catch (error) {
-      setMessage('Error updating grade');
-      console.error('There was an error!', error);
+      setMessage("Error updating grade");
+      console.error("There was an error!", error);
     }
   };
 
@@ -62,54 +65,81 @@ const Grades = () => {
       ) : error ? (
         <p>There was an error: {error.message}</p>
       ) : (
-        <table className="table table-bordered" style={{ maxHeight: '100vh' }}>
-        <thead>
-          <tr>
-            <th class="name" ></th>
-            <th colSpan="12" className="text-center flex-fill">ATTENDANCE</th>
-            <th></th>
-          </tr>
-          <tr>
-            <td></td>
-            <td class="inputgrade bg-success ">A1</td>
-            <td class="inputgrade bg-success ">A2</td>
-            <td class="inputgrade bg-success ">A3</td>
-            <td class="inputgrade bg-success ">A4</td>
-            <td class="inputgrade bg-success ">A5</td>
-            <td class="inputgrade bg-success ">A6</td>
-            <td class="inputgrade bg-success ">A7</td>
-            <td class="inputgrade bg-success ">A8</td>
-            <td class="inputgrade bg-success ">A9</td>
-            <td class="inputgrade bg-success "> A10</td>
-            <td class="inputgrade bg-success ">TOTAL</td>
-            <td class="inputgrade bg-success ">%</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {grades.map((grade) => (
-            <tr key={grade.gradeid}>
-              <td>{grade.name}</td>
-              {['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10'].map((field) => (
-                <td key={field}>
-                  <input
-                    type="number"
-                    name={field}
-                    value={formData[grade.gradeid]?.[field] ?? grade[field]}
-                    onFocus={(e) => handleInputFocus(e, grade.gradeid)}
-                    onChange={(e) => handleInputChange(e, grade.gradeid)}
-                  />
-                </td>
-              ))}
-              <td >{grade.a_total}</td>
-              <td>{grade.a_percentage}</td>
-              <td>
-                <button onClick={() => handleSave(grade.gradeid)}>Save</button>
-              </td>
+        <table className="table table-bordered" style={{ maxHeight: "100vh" }}>
+          <thead>
+            <tr>
+              <th class="name"></th>
+              <th colSpan="12" className="text-center flex-fill">
+                ATTENDANCE
+              </th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            <tr>
+              <td></td>
+              <td class="inputgrade bg-success text-light text-center ">A1</td>
+              <td class="inputgrade bg-success text-light text-center">A2</td>
+              <td class="inputgrade bg-success text-light text-center">A3</td>
+              <td class="inputgrade bg-success text-light text-center">A4</td>
+              <td class="inputgrade bg-success text-light text-center">A5</td>
+              <td class="inputgrade bg-success text-light text-center">A6</td>
+              <td class="inputgrade bg-success text-light text-center">A7</td>
+              <td class="inputgrade bg-success text-light text-center">A8</td>
+              <td class="inputgrade bg-success text-light text-center">A9</td>
+              <td class="inputgrade bg-success text-light text-center"> A10</td>
+              <td class="inputgrade bg-success text-light text-center">
+                TOTAL
+              </td>
+              <td class="inputgrade bg-success text-light text-center">%</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {grades.map((grade) => (
+              <tr key={grade.gradeid}>
+                <td>{grade.name}</td>
+                {[
+                  "a1",
+                  "a2",
+                  "a3",
+                  "a4",
+                  "a5",
+                  "a6",
+                  "a7",
+                  "a8",
+                  "a9",
+                  "a10",
+                ].map((field) => (
+                  <td key={field}>
+                    <input
+                      type="text"
+                      onInput={inputNumber}
+                      name={field}
+                      value={formData[grade.gradeid]?.[field] ?? grade[field]}
+                      onFocus={(e) => handleInputFocus(e, grade.gradeid)}
+                      onChange={(e) => handleInputChange(e, grade.gradeid)}
+                    />
+                  </td>
+                ))}
+                <td class="text-center">{grade.a_total}</td>
+                <td class="text-center">{grade.a_percentage}</td>
+                <td class="d-flex gap-2">
+                  <button
+                    onClick={() => handleSave(grade.gradeid)}
+                    class="btn btn-success"
+                  >
+                    Save
+                  </button>
+                  <button type="submit" class="btn btn-primary">
+                    Add
+                  </button>
+                  <button type="submit" class="btn btn-secondary">
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
       {message && <p>{message}</p>}
     </main>
